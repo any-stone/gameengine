93b947a60a1923f927243f031b3557b4ddf11579
Annie Kuxa <kuxa.business@gmail.com>
2026-02-10 22:12:02 -0400

    create and render SDL window.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,34 +1,79 @@
 #include "Game.h"
+#include <SDL2/SDL.h>
 #include <iostream>
 
 Game::Game() {
+    isRunning = false;
     std::cout << "Game constructor called!" << std::endl;
-};
+}
 
 Game::~Game() {
     std::cout << "Game destructor called!" << std::endl;
-};
+}
 
 void Game::Initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) !=0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    window = SDL_CreateWindow(
+            "The SoftNorth Presents", 
+            SDL_WINDOWPOS_CENTERED, 
+            SDL_WINDOWPOS_CENTERED,
+            800,
+            600,
+            SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI 
+     );
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
 
-};
-
-void Game::Run() {
-
-};
+    isRunning = true;
+}
 
 void Game::ProcessInput() {
-
-};
-
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {
+        switch (sdlEvent.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
+                break;
+        }
+    }
+} 
+      
 void Game::Update() {
 
-};
+}
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 25, 35, 255);
+    SDL_RenderClear(renderer);
 
-};
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Run() {
+    while (isRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    } 
+}
 
 void Game::Destroy() {
-
-};
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
--- src/Game.h
+++ src/Game.h
@@ -1,9 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
+#include <SDL2/SDL.h>
 
 class Game {
     private:
-        //...
+        bool isRunning;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
 
     public:
         Game();
